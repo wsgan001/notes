caefaf76c1798697a33031fb04a0e26ff058e264
dapeng.zhang <dapeng.zhang@zhangmen.com>
2017-07-07 18:59:52 +0800

    spring-websocket test

--- frameworks/spring-websocket/src/main/java/com/william/websocket/WebSocketConfig.java
+++ frameworks/spring-websocket/src/main/java/com/william/websocket/WebSocketConfig.java
@@ -1,6 +1,5 @@
-package com.william.config;
+package com.william.websocket;
 
-import com.william.handler.SpringWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,23 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-/**
- * Created by william on 17-7-6.
- */
 @Configuration
 @EnableWebMvc
 @EnableWebSocket
 public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(textMessageHandler(), "/websocket").addInterceptors(new HandshakeInterceptor());
+        registry.addHandler(textMessageHandler(), "/websocket/socketjs").addInterceptors(new HandshakeInterceptor()).withSockJS();
     }
 
     @Bean
-    public TextWebSocketHandler webSocketHandler() {
-        return new SpringWebSocketHandler();
+    public TextMessageHandler textMessageHandler() {
+        return new TextMessageHandler();
     }
 }
