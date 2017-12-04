508f1c408d15e2ccb77726edad900887fe5617a0
dapeng.zhang <dapeng.zhang@zhangmen.com>
2017-12-04 19:08:12 +0800

    add Wait_Join_DiffTest

--- python/src/main/curl/ranks-ajusted.py
+++ python/src/main/curl/ranks-ajusted.py
@@ -9,7 +9,7 @@ tuple_values = redis_conn.zrange(source_key, 0, -1)
 
 counter = 0
 for tuple_value in tuple_values:
-    cmd_format = 'curl --data "teaId=%s" '
+    cmd_format = 'curl --data "teaId=%s" https://chat.zmlearn.com/api/teacher/getMonthTeachStatus?access_token=c57cc5c1-855f-4c0a-a38f-cc02b90e5491'
     output = os.system(cmd_format % str(tuple_value))
     print(output)
     counter = (counter + 1)
